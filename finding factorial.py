aed1356ce34e597e040cbe171f665c039e676740
Just coding <asmaramy383@gmail.com>
2026-07-09 21:55:37 +0300

    Update finding factorial.py

--- finding factorial.py
+++ finding factorial.py
@@ -1,5 +1,5 @@
 def fact(number):
-    for i in range(1,number+1):
+    for i in range(1,number):
         number=number*i
     print("the factorial is",number)
 fact(int(input("enter a number: ")))
